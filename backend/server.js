067e264351b807220731f797a649007a3a92f915
miroslavvergov <gabberlt123@gmail.com>
2024-03-10 18:32:02 +0200

    Add backend folder where we first connect to a MongoDB collection

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from './db/connectDB.js'
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
