ca40e20851f721c99f4f63dd34e89e74908c3930
miroslavvergov <gabberlt123@gmail.com>
2024-03-11 21:29:48 +0200

    Introduce post functionality to the app and also add on a few modifications

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { followUnFollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js";
+import { followUnFollowUser, getUserProfile, loginUser, logoutUser, signupUser, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 //register
+router("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser)
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute ,followUnFollowUser);
-
+router.post("/update/:id", protectRoute, updateUser);
 
 export default router;
